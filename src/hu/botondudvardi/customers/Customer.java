8cab1338c362ef8aa80642d450bede26a36b346b
Botond.Udvardi <botond1119@gmail.com>
2020-03-25 10:51:36 +0100

    import data

--- src/hu/botondudvardi/customers/Customer.java
+++ src/hu/botondudvardi/customers/Customer.java
@@ -1,4 +1,45 @@
 package hu.botondudvardi.customers;
 
+import java.io.*;
+
 public class Customer {
+
+
+
+
+    public static void main(String[] args){
+
+        importData();
+    }
+
+    static void importData() {
+
+        BufferedReader br = null;
+
+
+        try {
+            br = new BufferedReader(new FileReader(new File("C:\\Users\\Botond\\ProjectMarch/ugyfelek.csv")));
+            String line;
+
+
+            while ((line = br.readLine()) != null) {
+
+                System.out.println(line);
+            }
+
+        } catch (FileNotFoundException fnfe) {
+
+            fnfe.printStackTrace();
+
+        } catch (IOException ioe) {
+
+            ioe.printStackTrace();
+        } finally {
+            try {
+                br.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
